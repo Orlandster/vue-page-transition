2373a334c8dbf091926d2bb0c299e31c376343cd
Orlando Wenzinger <orlando.wenzinger@bluewin.ch>
2019-04-08 16:59:43 +0200

    [Feature]: Add possibility for multiple transitions depending on route

--- src/components/VuePageTransition.vue
+++ src/components/VuePageTransition.vue
@@ -1,28 +1,175 @@
 <template>
-  <component :is="name">
+  <transition
+    :name="transition"
+    :mode="mode"
+  >
     <slot></slot>
-  </component>
+  </transition>
 </template>
 
 <script>
-import Fade from './transitions/Fade/Fade.vue'
-import FadeInUp from './transitions/Fade/FadeInUp.vue'
-import FadeInRight from './transitions/Fade/FadeInRight.vue'
-import FadeInDown from './transitions/Fade/FadeInDown.vue'
-import FadeInLeft from './transitions/Fade/FadeInLeft.vue'
-import Zoom from './transitions/Zoom/Zoom.vue'
-
 export default {
   name: 'vue-page-transition',
   props: ['name'],
-  components: {
-    Fade,
-    FadeInUp,
-    FadeInRight,
-    FadeInLeft,
-    FadeInDown,
-    Zoom,
+  data() {
+    return {
+      transition: 'fade',
+      mode: 'out-in',
+    }
   },
+  created() {
+    this.$router.beforeEach((to, from, next) => {
+      this.transition = this.$route.meta.transition
+        ? this.$route.meta.transition
+        : this.$props.name
+
+      next()
+    })
+  }
 }
 </script>
+
+<style lang="scss" scoped>
+/* Fade */
+.fade-enter-active,
+.fade-leave-active {
+  transition-duration: 0.3s;
+  transition-property: opacity;
+  transition-timing-function: ease;
+}
+.fade-enter,
+.fade-leave-active {
+  opacity: 0
+}
+
+/* Fade In Down */
+@keyframes fadeInDown {
+  from {
+    transform: translate3d(0, -40px, 0);
+  }
+
+  to {
+    transform: translate3d(0, 0, 0);
+    opacity: 1
+  }
+}
+
+.fade-in-down-leave-to {
+  opacity: 0;
+  transition: opacity .3s;
+}
+
+.fade-in-down-enter {
+  opacity: 0;
+  transform: translate3d(0, -40px, 0);
+}
+
+.fade-in-down-enter-to {
+  opacity: 0;
+  animation-duration: .7s;
+  animation-fill-mode: both;
+  animation-name: fadeInDown;
+}
+
+/* Fade In Right */
+@keyframes fadeInRight {
+  from {
+    transform: translate3d(40px, 0, 0);
+  }
+
+  to {
+    transform: translate3d(0, 0, 0);
+    opacity: 1
+  }
+}
+
+.fade-in-right-leave-to {
+  opacity: 0;
+  transition: opacity .3s;
+}
+
+.fade-in-right-enter {
+  opacity: 0;
+  transform: translate3d(40px, 0, 0);
+}
+
+.fade-in-right-enter-to {
+  opacity: 0;
+  animation-duration: .7s;
+  animation-fill-mode: both;
+  animation-name: fadeInRight;
+}
+
+/* Fade In Up */
+@keyframes fadeInUp {
+  from {
+    transform: translate3d(0, 40px, 0);
+  }
+
+  to {
+    transform: translate3d(0, 0, 0);
+    opacity: 1
+  }
+}
+
+.fade-in-up-leave-to {
+  opacity: 0;
+  transition: opacity .3s;
+}
+
+.fade-in-up-enter {
+  opacity: 0;
+  transform: translate3d(0, 40px, 0);
+}
+
+.fade-in-up-enter-to {
+  opacity: 0;
+  animation-duration: .7s;
+  animation-fill-mode: both;
+  animation-name: fadeInUp;
+}
+
+/* Fade In Left */
+@keyframes fadeInLeft {
+  from {
+    transform: translate3d(-40px, 0, 0);
+  }
+
+  to {
+    transform: translate3d(0, 0, 0);
+    opacity: 1
+  }
+}
+
+.fade-in-left-leave-to {
+  opacity: 0;
+  transition: opacity .3s;
+}
+
+.fade-in-left-enter {
+  opacity: 0;
+  transform: translate3d(-40px, 0, 0);
+}
+
+.fade-in-left-enter-to {
+  opacity: 0;
+  animation-duration: .7s;
+  animation-fill-mode: both;
+  animation-name: fadeInLeft;
+}
+
+/* Zoom */
+.zoom-enter-active,
+.zoom-leave-active {
+  transition-duration: 0.3s;
+  transition-property: all;
+  transition-timing-function: ease;
+}
+
+.zoom-enter,
+.zoom-leave-to {
+  opacity: 0;
+  transform: scale(0);
+}
+</style>
 
